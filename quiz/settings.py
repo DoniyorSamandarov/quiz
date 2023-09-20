b26a184b91d37254202b1cfc0f36fbf8397949cd
doni_1909 <samandarovdoniyor069@gmail.com>
2023-09-20 16:41:38 +0500

    fix

--- quiz/settings.py
+++ quiz/settings.py
@@ -81,10 +81,10 @@ env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 DATABASES = {
     'default': {
-        'ENGINE': env("DB_ENGINE"),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
         "HOST": env("DB_HOST"),
         "PORT": env("DB_PORT"),
     }
